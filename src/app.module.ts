fcd1efb9a6a47cb4452626eeb8fdb2bf96bf9bf9
Chris Casey <cdcasey@users.noreply.github.com>
2023-03-02 19:05:58 -0600

    Basic structure and auth service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookamrkModule } from './bookamrk/bookamrk.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UserModule, BookamrkModule],
+  // controllers: [],
+  // providers: [],
 })
 export class AppModule {}
