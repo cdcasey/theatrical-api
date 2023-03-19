1d32aeb9575a0ddc845c2ac339c315313dfb044e
Chris Casey <cdcasey@users.noreply.github.com>
2023-03-19 14:46:03 -0500

    Fix decorator

--- src/auth/decorator/get-user.decorator.ts
+++ src/auth/decorator/get-user.decorator.ts
@@ -2,10 +2,13 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const GetUser = createParamDecorator((data: string | undefined, ctx: ExecutionContext) => {
   const request: Express.Request = ctx.switchToHttp().getRequest();
+
+  // Get specific field passed to GetUser()
   if (data) {
-    // neat but dumb
+    // neat but dumb (maybe)
     // @ts-ignore
-    return request.user['data'];
+    return request.user[data];
   }
+
   return request.user;
 });
